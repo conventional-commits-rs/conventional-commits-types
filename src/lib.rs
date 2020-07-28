ec4109f00f02ca0c51cbb562aed72e11959064a6
Sven Lechner <SirWindfield@users.noreply.github.com>
2020-07-29 00:12:31 +0200

    fix!: optional components not using `Option`
    Beforehand, every component of a commit had to be present, including
    optional types. This commit defines optional types using `Option`.
    
    BREAKING CHANGE: The public struct property types change.
    
    Fixes #1

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,8 @@
 
 #[cfg(feature = "serde")]
 use serde::{Deserialize, Serialize};
+use std::fmt;
+use std::str::FromStr;
 
 /// A commit message.
 ///
@@ -11,17 +13,41 @@ use serde::{Deserialize, Serialize};
 #[derive(Clone, Debug, Default, Eq, Hash, PartialEq)]
 pub struct Commit<'a> {
     /// A body.
-    pub body: &'a str,
+    pub body: Option<&'a str>,
     /// A short summary.
     pub desc: &'a str,
-    /// A list of footers.
+    /// A list of footers. Empty when none.
     pub footer: Vec<Footer<'a>>,
     /// Set if the commit is a breaking change.
     pub is_breaking_change: bool,
-    /// The scope of the commit.
-    pub scope: &'a str,
+    /// The optional scope of the commit.
+    pub scope: Option<&'a str>,
     /// The type of the commit.
     pub ty: &'a str,
+}
+
+impl<'a> Commit<'a> {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn from(
+        ty: &'a str,
+        scope: Option<&'a str>,
+        desc: &'a str,
+        body: Option<&'a str>,
+        is_breaking_change: bool,
+        footer: Vec<Footer<'a>>,
+    ) -> Self {
+        Self {
+            ty,
+            scope,
+            desc,
+            body,
+            is_breaking_change,
+            footer,
+        }
+    }
 }
 
 /// A commit footer.
@@ -30,6 +56,61 @@ pub struct Commit<'a> {
 pub struct Footer<'a> {
     /// The footer word token.
     pub token: &'a str,
+    /// The separator.
+    pub separator: FooterSeparator,
     /// The footer's value.
     pub value: &'a str,
+}
+
+impl<'a> Footer<'a> {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn from(
+        token: &'a str,
+        separator: FooterSeparator,
+        value: &'a str,
+    ) -> Self {
+        Self {
+            token,
+            separator,
+            value
+        }
+    }
+}
+
+/// The separator used to separate the token and value of a footer.
+#[cfg_attr(feature = "serde", derive(Deserialize, Serialize))]
+#[derive(Copy, Clone, Debug, Eq, Hash, PartialEq)]
+pub enum FooterSeparator {
+    ColonSpace,
+    SpaceHashTag,
+}
+
+impl Default for FooterSeparator {
+    fn default() -> Self {
+        FooterSeparator::ColonSpace
+    }
+}
+
+impl fmt::Display for FooterSeparator {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            FooterSeparator::ColonSpace => write!(f, ": "),
+            FooterSeparator::SpaceHashTag => write!(f, " #"),
+        }
+    }
+}
+
+impl FromStr for FooterSeparator {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            ": " => Ok(FooterSeparator::ColonSpace),
+            " #" => Ok(FooterSeparator::SpaceHashTag),
+            _ => Err("footer separator not recognized".to_string())
+        }
+    }
 }
