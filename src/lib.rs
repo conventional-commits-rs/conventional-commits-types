3228532e4c330b1e9c34e01f49c0c884a1aee72d
Sven Lechner <SirWindfield@users.noreply.github.com>
2020-07-29 14:32:21 +0200

    docs: improve and add missing documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,42 +1,66 @@
-//! Defines structures that can be used to work with conventional-commits commit
-//! messages.
+//! Defines structures that can be used to work with conventional commits.
+//! The implementation resembles the v1.0.0 specification defined over at [conventionalcommits.org](https://www.conventionalcommits.org/en/v1.0.0/#specification).
 
 #[cfg(feature = "serde")]
 use serde::{Deserialize, Serialize};
-use std::fmt;
-use std::str::FromStr;
+use std::{fmt, str::FromStr};
 
 /// The `:<space>` separator.
 pub const SEPARATOR_COLON: &str = ": ";
 
 /// The `<space>#` separator for footer notes.
+///
+/// This type of separator is mostly used when using issues or PR numbers as
+/// value.
 pub const SEPARATOR_HASHTAG: &str = " #";
 
 /// A commit message.
 ///
-/// The different sections are separated by an empty newline.
+/// As per the specification, a commit message is made out of a mandatory
+/// description, an optional body and `0..n` optional footers. The different
+/// sections are separated by an empty newline. Footers can be each separated
+/// with a newline, however, this is not needed.
+///
+/// # Example
+///
+/// ```text
+/// feat(some scope): a short and concise description
+///
+/// This is a longer body message. It can wrapped around
+/// and be put onto multiple lines.
+///
+/// This is still part of the body.
+///
+/// Fixes #123
+/// PR-close #124
+/// Signed-off-by: SirWindfield
+/// ```
 #[cfg_attr(feature = "serde", derive(Deserialize, Serialize))]
 #[derive(Clone, Debug, Default, Eq, Hash, PartialEq)]
 pub struct Commit<'a> {
-    /// A body.
+    /// The optional body.
     pub body: Option<&'a str>,
-    /// A short summary.
+    /// The mandatory description.
     pub desc: &'a str,
-    /// A list of footers. Empty when none.
+    /// A list of footers. Empty when none are part of the commit message.
     pub footer: Vec<Footer<'a>>,
     /// Set if the commit is a breaking change.
     pub is_breaking_change: bool,
-    /// The optional scope of the commit.
+    /// The optional scope.
     pub scope: Option<&'a str>,
-    /// The type of the commit.
+    /// The mandatory type.
+    ///
+    /// Types other than `feat` and `fix` are optional. For more information, please take a look at the [specification](https://www.conventionalcommits.org/en/v1.0.0/#specification), paragraphs 1-3.
     pub ty: &'a str,
 }
 
 impl<'a> Commit<'a> {
+    /// Creates a default commit.
     pub fn new() -> Self {
         Self::default()
     }
 
+    /// Creates a commit with the given values.
     pub fn from(
         ty: &'a str,
         scope: Option<&'a str>,
@@ -56,7 +80,7 @@ impl<'a> Commit<'a> {
     }
 }
 
-/// A commit footer.
+/// A commit message footer.
 #[cfg_attr(feature = "serde", derive(Deserialize, Serialize))]
 #[derive(Clone, Debug, Default, Eq, Hash, PartialEq)]
 pub struct Footer<'a> {
@@ -69,10 +93,12 @@ pub struct Footer<'a> {
 }
 
 impl<'a> Footer<'a> {
+    /// Creates a default footer.
     pub fn new() -> Self {
         Self::default()
     }
 
+    /// Creates a footer with the given values.
     pub fn from(token: &'a str, separator: FooterSeparator, value: &'a str) -> Self {
         Self {
             token,
@@ -86,11 +112,16 @@ impl<'a> Footer<'a> {
 #[cfg_attr(feature = "serde", derive(Deserialize, Serialize))]
 #[derive(Copy, Clone, Debug, Eq, Hash, PartialEq)]
 pub enum FooterSeparator {
+    /// The `:<space>` separator is mostly used for values that do not involve
+    /// issue or PR references.
     ColonSpace,
+    /// The `#<space>` separator is, more often than not, used for issue and PR
+    /// references.
     SpaceHashTag,
 }
 
 impl Default for FooterSeparator {
+    /// Returns the default FooterSeparator, the ColonSpace.
     fn default() -> Self {
         FooterSeparator::ColonSpace
     }
